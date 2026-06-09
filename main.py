e5529014d901f167fe1f3773c7d9f9ee4518dcf8
Ali Rahimi <ali.rahimi@gandom.co>
2026-06-09 19:56:48 +0200

    query params and together with path

--- main.py
+++ main.py
@@ -35,3 +35,21 @@ async def read_all_books(book_title: str):
         if book['title'].casefold() == book_title.casefold():
             return book
     return None
+
+# Query Params
+@app.get("/books/")
+async def read_books_by_category(category: str):
+    result = []
+    for book in BOOKS:
+        if book['category'].casefold() == category.casefold():
+            result.append(book)
+    return result
+
+# Query and path params
+@app.get("/books/{author}/")
+async def read_books_by_author_and_category(author: str, category: str):
+    result = []
+    for book in BOOKS:
+        if book['author'].casefold() == author.casefold() and book['category'].casefold() == category.casefold():
+            result.append(book)
+    return result
