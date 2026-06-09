eef411aca349a4f34a1f287719e6f802210dd1a3
Ali Rahimi <ali.rahimi@gandom.co>
2026-06-09 19:46:40 +0200

    path parameters

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
@@ -12,7 +11,27 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
 
+
+# @app.get("/books/{dynamic_params}")
+# async def read_all_books(dynamic_params: str):
+#     return {"dynamic_params": dynamic_params}
+
+
+# order matter !!!
+# @app.get("/books/mybook")
+# async def read_all_books():
+#     return {"books title": "my favorite book"}
+
+
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book['title'].casefold() == book_title.casefold():
+            return book
+    return None
