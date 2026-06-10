1145be2433b2a60baa06ba4a27b6073ae7b8bfe3
Ali Rahimi <ali.rahimi@gandom.co>
2026-06-10 11:35:23 +0200

    pydantic fields

--- books.py
+++ books.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from main import BOOKS
 
@@ -20,11 +22,11 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 app = FastAPI()
@@ -47,4 +49,12 @@ async def read_all_books():
 @app.post("/books/new-book/}")
 async def create_new_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    if len(BOOKS) > 0:
+        book_id = BOOKS[-1].id + 1
+    else:
+        book_id = 1
+    return book
